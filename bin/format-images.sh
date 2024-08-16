cd63ef610a941dc38b9d4541d82318f2d2ab0f86
Billiam <billiamthesecond@gmail.com>
2024-08-16 13:21:49 -0500

    Add verbose flag

--- bin/format-images.sh
+++ bin/format-images.sh
@@ -2,4 +2,4 @@
 
 : ${1?' directory name is required'}
 
-mogrify -path "$1/.." -strip -channel rgb -contrast-stretch 0.1%x1% -adaptive-resize 1200x1200\> -quality 90% -format jpg "$1/*.jpg"
+mogrify -path "$1/.." -strip -channel rgb -contrast-stretch 0.1%x1% -adaptive-resize 1200x1200\> -quality 90% -verbose -format jpg "$1/*.jpg"
